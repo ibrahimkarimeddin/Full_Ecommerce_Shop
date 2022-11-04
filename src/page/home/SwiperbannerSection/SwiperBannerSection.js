ffd6f76a5c7b31d5482aa22ccfd1fd64ed8dd306
ibrahim karim eddin <ibrahimkarimeddin@gmail.com>
2022-11-04 23:52:33 +0200

    comment

--- src/page/home/SwiperbannerSection/SwiperBannerSection.js
+++ src/page/home/SwiperbannerSection/SwiperBannerSection.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-import SwiperCore, { Autoplay } from 'swiper';
+import { Navigation, Pagination, Scrollbar, A11y,Autoplay } from 'swiper';
+
 
 function SwiperBannerSection() {
-    SwiperCore.use([Autoplay])
   return (
     <Swiper
    
@@ -16,8 +15,9 @@ function SwiperBannerSection() {
     }}
       spaceBetween={0}
       slidesPerView={1}
-      swipeHandler
       
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
     >
       <SwiperSlide><img src='/swiperImage/image.jpg ' alt="swiper_image" className='mx-auto w-[90vw] h-[25vh] md:w-[55vw] md:h-[55vh] rounded-lg' /></SwiperSlide>
       <SwiperSlide><img src='/swiperImage/image2.jpg ' alt="swiper_image" className='mx-auto w-[90vw] h-[25vh] md:w-[55vw] md:h-[55vh] rounded-lg' /></SwiperSlide>
